e6c8f7247f7bb7c9a1503ae394520bac77618781
Aren Ansper <aren.ansper@gmail.com>
2024-05-10 09:24:20 +0300

    session

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Redirect;
-use Session;
+use Illuminate\Support\Facades\Session;
 
 class CheckoutController extends Controller
 {
